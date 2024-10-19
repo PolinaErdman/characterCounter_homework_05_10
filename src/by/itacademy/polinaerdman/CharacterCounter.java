5aa4af19d8eecda8f896ecbc5d37b60c3c9951bd
palinaerdman <polinaerdman@gmail.com>
2024-10-19 19:12:41 +0300

    add class CharacterCounter

---
+++ src/by/itacademy/polinaerdman/CharacterCounter.java
@@ -0,0 +1,27 @@
+package by.itacademy.polinaerdman;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String text = scanner.nextLine();
+        scanner.close();
+
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        for (char ch : text.toCharArray()) {
+            if (charMap.containsKey(ch))
+                charMap.replace(Character.valueOf(ch), charMap.get(ch) + 1);
+            else
+                charMap.put(Character.valueOf(ch), 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charMap.entrySet()) {
+            System.out.println("Количество букв " + entry.getKey() + " равняется " + entry.getValue());
+        }
+    }
+}
+
